1352613020cbfc6edfc0e3077f3423eca25a3401
ahsan.habib01 <ahshapon00@gmail.com>
2025-11-21 15:53:16 +0600

    add stripe payment gateway

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -12,6 +12,8 @@ import SendParcel from '../Pages/SendParcel/SendParcel';
 import DashboardLayout from '../Layouts/DashboardLayout';
 import MyParcels from '../Pages/Dashboard/MyParcels/MyParcels';
 import Payment from '../Pages/Dashboard/Payment/Payment';
+import PaymentSuccess from '../Pages/Dashboard/Payment/PaymentSuccess';
+import PaymentCancelled from '../Pages/Dashboard/Payment/PaymentCancelled';
 
 const router = createBrowserRouter([
   {
@@ -81,7 +83,15 @@ const router = createBrowserRouter([
         path: 'payment/:parcelId',
         Component: Payment,
       },
-    ]
+      {
+        path: 'payment-success',
+        Component: PaymentSuccess,
+      },
+      {
+        path: 'payment-cancelled',
+        Component: PaymentCancelled,
+      },
+    ],
   },
 ]);
 
