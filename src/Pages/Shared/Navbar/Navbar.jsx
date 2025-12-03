11890f2fb430048c43cb57a549bf9faf7302e0e7
ahsan.habib01 <ahshapon00@gmail.com>
2025-12-03 23:24:42 +0600

    change route

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
       {user && (
         <>
           <li>
-            <NavLink to="/dashboard/my-parcels">My Parcels</NavLink>
+            <NavLink to="/dashboard/my-parcels">My Parcel</NavLink>
           </li>
         </>
       )}
