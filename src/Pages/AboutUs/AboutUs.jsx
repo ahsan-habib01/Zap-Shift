0db6df12b49dced81495ddc9f8275ee0b55ee047
ahsan.habib01 <ahshapon00@gmail.com>
2026-02-18 00:59:00 +0600

    changes in about us page

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -6,7 +6,7 @@ const content = {
   Story: `
 We started with a simple promise — to make parcel delivery fast, reliable, and stress-free. Over the years, our commitment to real-time tracking, efficient logistics, and customer-first service has made us a trusted partner for thousands. Whether it's a personal gift or a time-sensitive business delivery, we ensure it reaches its destination — on time, every time.
 
-We started with a simple promise — to make parcel delivery fast, reliable, and stress-free. Over the years, our commitment to real-time tracking, efficient logistics, and customer-first service has made us a trusted partner for thousands. Whether it's a personal gift or a time-sensitive business delivery, we ensure it reaches its destination — on time, every time.
+We started with a simple promise — to make parcel delivery fast, reliable, and stress-free. Over the years, our commitment to real-time tracking, efficient logistics, and customer-first service has made us a trusted partner for thousands. Whether it is a personal gift or a time-sensitive business delivery, we ensure it reaches its destination — on time, every time.
 
 We started with a simple promise — to make parcel delivery fast, reliable, and stress-free. Over the years, our commitment to real-time tracking, efficient logistics, and customer-first service has made us a trusted partner for thousands. Whether it's a personal gift or a time-sensitive business delivery, we ensure it reaches its destination — on time, every time.
 `,
